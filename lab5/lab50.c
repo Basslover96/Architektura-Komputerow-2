8daf783194bf1c5f7a3b4c11c71055d39036c9c2
Basslover <dobrysiewiczk@interia.pl>
2018-05-16 10:06:56 +0200

    Lab5 - Koniec

--- lab5/lab50.c
+++ lab5/lab50.c
@@ -8,9 +8,10 @@ int main(){
 
     MyVector a,b,c,d,etap12,etap3;
     float wyznacznik=0;
+    float etap41,etap42;
 
     //Dane macierzy
-    a.s1=1;
+    a.s1=0;
     a.s2=2;
     a.s3=4;
     a.s4=5;
@@ -18,14 +19,14 @@ int main(){
     b.s1=7;
     b.s2=2;
     b.s3=5;
-    b.s4=1;
+    b.s4=-2;
 
     c.s1=3;
     c.s2=2;
     c.s3=5;
     c.s4=1;
 
-    d.s1=3;
+    d.s1=7;
     d.s2=8;
     d.s3=4;
     d.s4=1;
@@ -34,65 +35,72 @@ int main(){
         //---Etap 1---
         "movups %1,%%xmm0\n"
         "movups %2,%%xmm1\n"
-        //a0,a2,a0,a1 01 00 10 00
+        //01 00 10 00
         "shufps $0x48,%%xmm0,%%xmm0\n"
-        //b1,b0,b3,b2 10 11 00 01
+        //10 11 00 01
         "shufps $0xB1,%%xmm1,%%xmm1\n"
         "mulps %%xmm0,%%xmm1\n"
+
         "movups %1,%%xmm2\n"
         "movups %2,%%xmm3\n"
-        //a1,a0,a3,a2 10 11 00 01
+        //10 11 00 01
         "shufps $0xB1,%%xmm2,%%xmm2\n"
-        //b0,b2,b0,b1 01 00 10 00
+        //01 00 10 00
         "shufps $0x48,%%xmm3,%%xmm3\n"
         "mulps %%xmm2,%%xmm3\n"
         "subps %%xmm3,%%xmm1\n"
         "movups %%xmm1,%%xmm0\n"
+
+
         //---Etap 2---
         "movups %3,%%xmm1\n"
         "movups %4,%%xmm2\n"
-        //c2,c1,c1,c0 00 01 01 10
+        //00 01 01 10
         "shufps $0x16,%%xmm1,%%xmm1\n"
-        //d3,d3,d2,d3 11 10 11 11
+        //11 10 11 11
         "shufps $0xEF,%%xmm2,%%xmm2\n"
         "mulps %%xmm1,%%xmm2\n"
+
         "movups %3,%%xmm3\n"
         "movups %4,%%xmm4\n"
-        //c3,c3,c2,c3 11 10 11 11
+        //11 10 11 11
         "shufps $0xEF,%%xmm3,%%xmm3\n"
-        //d2,d1,d1,d0 00 01 01 10
+        //00 01 01 10
         "shufps $0x16,%%xmm4,%%xmm4\n"
         "mulps %%xmm3,%%xmm4\n"
         "subps %%xmm4,%%xmm2\n"
         "movups %%xmm2,%%xmm1\n"
+
         "mulps %%xmm0,%%xmm1\n"
         "movups %%xmm1, %0\n"
         : "=g"(etap12)
         : "g"(a),"g"(b),"g"(c),"g"(d)
     );
 
+    //---Etap 3---
     __asm__(
-        //---Etap 3---
         "movups %1,%%xmm2\n"
         "movups %2,%%xmm3\n"
         "movups %3,%%xmm4\n"
         "movups %4,%%xmm5\n"
-        //a2,a3,c0,c0 00 00 11 10
+        //00 00 11 10
         "shufps $0xE, %%xmm4,%%xmm2\n"
-        //b3,b1,d1,d2 10 01 01 11
+        //10 01 01 11
         "shufps $0x97, %%xmm5,%%xmm3\n"
         "mulps %%xmm2,%%xmm3\n"
         "movups %%xmm3, %%xmm2\n"
+
         "movups %1,%%xmm3\n"
         "movups %2,%%xmm4\n"
         "movups %3,%%xmm5\n"
         "movups %4,%%xmm6\n"
-        //a3,a1,c1,c2 10 01 01 11
+        //10 01 01 11
         "shufps $0x97, %%xmm5,%%xmm3\n"
-        //b2,b3,d0,d0 00 00 11 10
+        //00 00 11 10
         "shufps $0xE, %%xmm6,%%xmm4\n"
         "mulps %%xmm3,%%xmm4\n"
         "movups %%xmm4, %%xmm3\n"
+
         "subps %%xmm2,%%xmm3\n"
         "movups %%xmm3, %0\n"
         : "=g"(etap3)
@@ -100,8 +108,33 @@ int main(){
     );
 
     //---Etap 4---
-    wyznacznik=etap12.s1+etap12.s2+etap12.s3+etap12.s4+etap3.s1*etap3.s3+etap3.s2*etap3.s4;
+    __asm__(
+        "fld %1\n"
+        "fmul %2\n"
+        : "=t"(etap41)
+        : "m"(etap3.s1),"m"(etap3.s3)
+    );
 
+    __asm__(
+        "fld %1\n"
+        "fmul %2\n"
+        : "=t"(etap42)
+        : "m"(etap3.s2),"m"(etap3.s4)
+    );
+
+    //---Etap 5---
+    __asm__(
+        "fld %1\n"
+        "fadd %2\n"
+        "fadd %3\n"
+        "fadd %4\n"
+        "fadd %5\n"
+        "fadd %6\n"
+        : "=t"(wyznacznik)
+        : "m"(etap12.s1),"m"(etap12.s2),"m"(etap12.s3),"m"(etap12.s4),"m"(etap41),"m"(etap42)
+    );
+
+    //wyznacznik=etap12.s1+etap12.s2+etap12.s3+etap12.s4+etap3.s1*etap3.s3+etap3.s2*etap3.s4;
     printf("Wyznacznik wynosi: %f.\n",wyznacznik);
 
     return 0;
